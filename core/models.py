7b6338f052ccecf886aa6f3e66057aaac1d9c4c4
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-07-20 09:33:57 -0300

    adjusting auth and tables

--- core/models.py
+++ core/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth import models as auth_models
 
 
 class User(models.Model):
-    name = models.CharField(max_length=150, blank=False)
-    email = models.EmailField(unique=True, blank=False)
-    password = models.CharField(max_length=50, blank=False)
-    zip_code = models.CharField(max_length=50, blank=False)
+    name = models.CharField(max_length=150, blank=True, null=True)
+    email = models.EmailField(unique=True, blank=False, null=False)
+    password = models.CharField(max_length=50, blank=False, null=False)
+    zip_code = models.CharField(max_length=50, blank=True, null=True)
     owner = models.ForeignKey(auth_models.User, related_name="users", on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -16,9 +16,9 @@ class User(models.Model):
 
 
 class List(models.Model):
-    title = models.CharField(max_length=150, blank=False)
+    title = models.CharField(max_length=150, blank=False, null=False)
     user = models.ForeignKey(User, related_name="lists", on_delete=models.CASCADE)
-    products = models.JSONField(default=list, blank=True)
+    products = models.JSONField(default=list, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
