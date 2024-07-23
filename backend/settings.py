d00f82a3f61601ba5e2d1f03120c07d24769af89
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-07-23 20:28:17 -0300

    removed jwt auth (i dont want to use "heavy stateless" auth)

--- backend/settings.py
+++ backend/settings.py
@@ -41,14 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "core",
     "rest_framework",
-    "rest_framework_simplejwt",
 ]
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
-}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
