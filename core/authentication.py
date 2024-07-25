11895868b1aa9f161ec1076c40a2a8d18d09bec4
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-07-24 22:21:36 -0300

    authentication.py - custom auth params funcion DISCONTINUED

--- core/authentication.py
+++ core/authentication.py
@@ -1,18 +1,21 @@
-from django.contrib.auth.backends import BaseBackend
-from django.contrib.auth.models import User
-from django.core.exceptions import ObjectDoesNotExist
+""" DISCONTINUED IDEA """
 
-class EmailBackend(BaseBackend):
-    def authenticate(self, request, email=None, password=None, **kwargs):
-        try:
-            user = User.objects.get(email=email)
-            if user.check_password(password):
-                return user
-        except ObjectDoesNotExist:
-            return None
 
-    def get_user(self, user_id):
-        try:
-            return User.objects.get(pk=user_id)
-        except ObjectDoesNotExist:
-            return None
+# from django.contrib.auth.backends import BaseBackend
+# from django.contrib.auth.models import User
+# from django.core.exceptions import ObjectDoesNotExist
+
+# class EmailBackend(BaseBackend):
+#     def authenticate(self, request, email=None, password=None, **kwargs):
+#         try:
+#             user = User.objects.get(email=email)
+#             if user.check_password(password):
+#                 return user
+#         except ObjectDoesNotExist:
+#             return None
+
+#     def get_user(self, user_id):
+#         try:
+#             return User.objects.get(pk=user_id)
+#         except ObjectDoesNotExist:
+#             return None
