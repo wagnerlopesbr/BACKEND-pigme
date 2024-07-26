236e81d4745c40eecc1e99988952138534159469
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-07-26 14:34:52 -0300

    messaging v2 - create, update, delete

--- core/utils.py
+++ core/utils.py
@@ -40,26 +40,30 @@ def update_account(account, serializer, user):
     """Updates an account with the given serializer data."""
     if account.user != user:
         raise PermissionDenied("You do not have permission to update this account.")
+    serializer.is_valid(raise_exception=True)
     serializer.save(user=account.user)
     return account
 
 def delete_account(account, user):
     """Deletes an account and associated user."""
-    if account.user != user:
+    if account.user.id != user.id:
         raise PermissionDenied("You do not have permission to delete this account.")
+    account = Account.objects.get(user=user)
     account.user.delete()
-    return account
 
 def update_list(list_instance, account, serializer):
     """Updates a list with the given serializer data."""
-    if list_instance.account != account:
+    if list_instance.account.id != account.id:
+        print(f"Permission denied: list_instance.account={list_instance.account.id}, account={account.id}")
         raise PermissionDenied("You do not have permission to update this list.")
+    serializer.is_valid(raise_exception=True)
     updated_list = serializer.save(account=list_instance.account)
     return updated_list
 
 def delete_list(list_instance, account):
     """Deletes a list."""
-    if list_instance.account != account:
+    if list_instance.account.id != account.id:
+        print(f"Permission denied: list_instance.account={list_instance.account.id}, account={account.id}")
         raise PermissionDenied("You do not have permission to delete this list.")
     list_instance.delete()
     return list_instance
